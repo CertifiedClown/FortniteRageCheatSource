db815f496aca4b014d64808f49dafeb1aa728076
VITAL <81172363+VITAL1310@users.noreply.github.com>
2021-08-19 12:16:20 +0000

    Update dllmain.cpp

--- 1337/dllmain.cpp
+++ 1337/dllmain.cpp
@@ -225,7 +225,7 @@ HRESULT present_hooked(IDXGISwapChain* pSwapChain, UINT SyncInterval, UINT Flags
 	{
 		no_menu::window_input(_("UnrealWindow"), _("Fortnite  "));
 		                             // ur  menu name
-		if (no_menu::begin_window(_("VITAL PASTE"), no_menu::vec2({ 545, 580 }), VK_INSERT, no_menu::no_menu_window_flags_none))
+		if (no_menu::begin_window(_("1310's PASTE"), no_menu::vec2({ 545, 580 }), VK_INSERT, no_menu::no_menu_window_flags_none))
 		{
 
 			if (tab == 1) { tab1 = true; tab2 = false; tab3 = false; tab4 = false; }
