4a084864e005481dcc9c95721e1ced1286f51065
Santaa <81172363+SantaaVibez@users.noreply.github.com>
2022-01-23 05:33:55 -0800

    Update no_define.h

--- 1337/no_define.h
+++ 1337/no_define.h
@@ -19,19 +19,19 @@ vec3 LocalPawnRelativeLocation;
 float FOVAngle;
 vec3 CamLoc;
 vec3 CamRot;
-
+//updated for chapter 3
 namespace signatures {
-	const char* Uworld_Sig = "48 8B 05 ? ? ? ? 4D 8B C2"; //uworld
+	const char* Uworld_Sig = "48 89 05 ? ? ? ? 48 8B 4B 78"; //uworld
 
 	const char* Gobject_Sig = "48 8B 05 ? ? ? ? 48 8B 0C C8 48 8B 04 D1";
 
 	const char* Free_Sig = "48 85 C9 0F 84 ? ? ? ? 53 48 83 EC 20 48 89 7C 24 30 48 8B D9 48 8B 3D ? ? ? ? 48 85 FF 0F 84 ? ? ? ? 48 8B 07 4C 8B 40 30 48 8D 05 ? ? ? ? 4C 3B C0"; //Free
 
-	const char* ProjectWorldToScreen_Sig = "E8 ? ? ? ? 41 88 07 48 83 C4 30"; //ProjectWorldToScreen
+	const char* ProjectWorldToScreen_Sig = "E8 ? ? ? ? 48 8B 5C 24 ? 41 88 07 48 83 C4 30"; //ProjectWorldToScreen
 
-	const char* LineOfSightTo_Sig = "E8 ? ? ? ? 48 8B 0D ? ? ? ? 33 D2 40 8A F8"; //LineOfSight
+	const char* LineOfSightTo_Sig = "48 8B C4 48 89 58 20 55 56 57 41 54 41 55 41 56 41 57 48 8D 6C 24 ? 48 81 EC ? ? ? ? 0F 29 70 B8 0F 29 78 A8 44 0F 29 40 ? 48 8B 05 ? ? ? ? 48 33 C4 48 89 45 20 45 8A E9"; //LineOfSight
 
-	const char* GetNameByIndex_Sig = "48 89 5C 24 ? 48 89 74 24 ? 55 57 41 56 48 8D AC 24 ? ? ? ? 48 81 EC ? ? ? ? 48 8B 05 ? ? ? ? 48 33 C4 48 89 85 ? ? ? ? 45 33 F6 48 8B F2 44 39 71 04 0F 85 ? ? ? ? 8B 19 0F B7 FB E8 ? ? ? ? 8B CB 48 8D 54 24"; //GetNameByIndex
+	const char* GetNameByIndex_Sig = "48 89 5C 24 ? 48 89 6C 24 ? 56 57 41 56 48 81 EC ? ? ? ? 48 8B 05 ? ? ? ? 48 33 C4 48 89 84 24 ? ? ? ? 48 8B F2 4C 8B F1 E8 ? ? ? ? 45 8B 06 33 ED 41 0F B7 16 41 C1 E8 10 89 54 24 24 44 89 44 24 ? 48 8B 4C 24 ? 48 C1 E9 20 8D 3C 09 4A 03 7C C0 ? 0F B7 17 C1 EA 06 41 39 6E 04"; //GetNameByIndex
 
 	const char* BoneMatrix_Sig = "E8 ? ? ? ? 48 8B 47 30 F3 0F 10 45"; //BoneMatrix
 
@@ -43,35 +43,35 @@ namespace signatures {
 }
 	
 namespace offsets {
-	uintptr_t OwningGameInstance = 0x180;
+	uintptr_t OwningGameInstance = 0x190;
 	uintptr_t LocalPlayers = 0x38;
 	uintptr_t PlayerController = 0x30;
-	uintptr_t PlayerCameraManager = 0x2B8;
-	uintptr_t AcknowledgedPawn = 0x2A0;
+	uintptr_t PlayerCameraManager = 0x2C8;
+	uintptr_t AcknowledgedPawn = 0x2B0;
 
-	uintptr_t Levels = 0x138;
+	uintptr_t Levels = 0x148;
 	uintptr_t PersistentLevel = 0x30;
 	uintptr_t AActors = 0x98;
 	uintptr_t ActorCount = 0xA0;
 
 	uintptr_t ComponentVelocity = 0x140;
-	uintptr_t RootComponent = 0x130;
-	uintptr_t FireStartLoc = 0x878;
-	uintptr_t RelativeLocation = 0x11c;
+	uintptr_t RootComponent = 0x138;
+	uintptr_t FireStartLoc = 0x8C8;
+	uintptr_t RelativeLocation = 0x11C;
 	uintptr_t RelativeRotation = 0x128;
-	uintptr_t CurrentWeapon = 0x600;
+	uintptr_t CurrentWeapon = 0x5F8;
 	uintptr_t PlayerState = 0x240;
-	uintptr_t Mesh = 0x280;
-	uintptr_t TeamIndex = 0xED0;
+	uintptr_t Mesh = 0x288;
+	uintptr_t TeamIndex = 0xF30;
 
 
 	//Exploits
 	uintptr_t bDisableEquipAnimation = 0x2B4;
-	uintptr_t bADSWhileNotOnGround = 0x3AB1;
-	uintptr_t ReviveFromDBNOTime = 0x3420;
-	uintptr_t CustomTimeDilation = 0x538;
-	uintptr_t LastFireTime = 0x9BC;
-	uintptr_t LastFireTimeVerified = 0x9C0;
+	uintptr_t bADSWhileNotOnGround = 0x3E61;
+	uintptr_t ReviveFromDBNOTime = 0x3768;
+	uintptr_t CustomTimeDilation = 0x9C;
+	uintptr_t LastFireTime = 0x9EC;
+	uintptr_t LastFireTimeVerified = 0x9F0;
 
 }
 
